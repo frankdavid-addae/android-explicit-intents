0093d640c8d6987fddc3e3a83aacd8ae889dc00b
Frank David Addae <frankdavid.addae@gmail.com>
2021-09-28 17:56:29 +0000

    added new activities

--- app/src/main/java/com/example/androidexplicitintent/MainActivity.java
+++ app/src/main/java/com/example/androidexplicitintent/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.androidexplicitintent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etName;
     Button btnAct2, btnAct3;
     TextView tvResults;
+
+    final int ACTIVITY3 = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +32,39 @@ public class MainActivity extends AppCompatActivity {
         btnAct2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (etName.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                } else {
+                    String name = etName.getText().toString().trim();
 
+                    Intent intent = new Intent(MainActivity.this, com.example.androidexplicitintent.Activity2.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }
             }
         });
 
         btnAct3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, com.example.androidexplicitintent.Activity3.class);
+                startActivityForResult(intent, ACTIVITY3);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ACTIVITY3) {
+            if (resultCode == RESULT_OK) {
+                tvResults.setText(data.getStringExtra("surname"));
+            }
+
+            if (resultCode == RESULT_CANCELED) {
+                tvResults.setText("No data received!");
+            }
+        }
     }
 }
